8e358f359a60bd75d4af8b905810c5ceff51e086
Rebecca Richards <rrichards@earnestresearch.com>
2022-02-19 16:43:11 +0000

    feat: add ability to specify what variations get returned from getAllVariations

--- src/runtime/composables/useLaunchDarkly.ts
+++ src/runtime/composables/useLaunchDarkly.ts
@@ -1,9 +1,5 @@
-import {
-  LDEvaluationDetail,
-  LDFlagsState,
-  LDUser
-} from 'launchdarkly-node-server-sdk'
-import { useRuntimeConfig, useFetch } from '#app'
+import { LDEvaluationDetail, LDUser } from 'launchdarkly-node-server-sdk'
+import { useRuntimeConfig, useFetch, AsyncData } from '#app'
 
 export interface LDVariation {
   variation: boolean
@@ -13,11 +9,21 @@ export const useLaunchDarkly = () => {
   /**
    * Fetches all the variations for the provided user
    * @param user LDUser
+   * @param includeKeys Specify which variant keys you want returned. If undefined, all keys will be returned.
+   * @param key A unique key to ensure that data fetching can be properly de-duplicated across requests.
+   * Use if getAllVariations is used more than once per component
+   * @returns
    */
-  const getAllVariations = (user: LDUser) => {
+  const getAllVariations = (
+    user: LDUser,
+    includeKeys?: string[],
+    key?: string
+  ) => {
     const { launchDarkly } = useRuntimeConfig()
-    const res = useFetch<LDFlagsState>(launchDarkly.apiPath, {
-      params: user
+    const res = useFetch<Map<string, boolean>>(launchDarkly.apiPath, {
+      params: user,
+      pick: includeKeys,
+      ...(key && { key })
     })
     return res
   }
@@ -26,15 +32,20 @@ export const useLaunchDarkly = () => {
    * Fetches a single variation for the provided user
    * @param user LDUser
    * @param flagKey string
+   * @param defaultValue boolean
+   * @param key A unique key to ensure that data fetching can be properly de-duplicated across requests.
+   * Use if getVariationByKey is used more than once per component
    */
   const getVariationByKey = (
     user: LDUser,
     flagKey: string,
-    defaultValue: boolean = false
+    defaultValue: boolean = false,
+    key?: string
   ) => {
     const { launchDarkly } = useRuntimeConfig()
     const res = useFetch<LDVariation>(`${launchDarkly.apiPath}/${flagKey}`, {
-      params: { ...user, defaultValue: defaultValue.toString() }
+      params: { ...user, defaultValue: defaultValue.toString() },
+      ...(key && { key })
     })
     return res
   }
@@ -43,17 +54,22 @@ export const useLaunchDarkly = () => {
    * Fetches a single variation for the provided user with detail
    * @param user LDUser
    * @param flagKey string
+   * @param defaultValue boolean
+   * @param key A unique key to ensure that data fetching can be properly de-duplicated across requests.
+   * Use if getVariationByKey is used more than once per component
    */
   const getVariationDetail = (
     user: LDUser,
     flagKey: string,
-    defaultValue: boolean = false
+    defaultValue: boolean = false,
+    key?: string
   ) => {
     const { launchDarkly } = useRuntimeConfig()
     const res = useFetch<LDEvaluationDetail>(
       `${launchDarkly.apiPath}/${flagKey}/detail`,
       {
-        params: { ...user, defaultValue: defaultValue.toString() }
+        params: { ...user, defaultValue: defaultValue.toString() },
+        ...(key && { key })
       }
     )
     return res
